34e1daf2f88787047e488a7092bf4cc11b6434bf
Paul DE LA CELLE <Paul.DELACELLE@sinequa.com>
2025-12-28 14:28:24 +0100

    fix: deploy dist/client with node adapter workaround

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,20 @@ import tailwindcss from '@tailwindcss/vite';
 import keystatic from '@keystatic/astro';
 import react from '@astrojs/react';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+
   // output: 'static' is the default in Astro 5
   integrations: [keystatic(), react()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
